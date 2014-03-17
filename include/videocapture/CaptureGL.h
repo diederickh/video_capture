682da1a2bd4ab8d6c649888718aef018864b17dd
roxlu <diederick@apollomedia.nl>
2014-03-17 21:15:58 +0100

    Ported the CaptureGL to linux

--- include/videocapture/CaptureGL.h
+++ include/videocapture/CaptureGL.h
@@ -3,11 +3,46 @@
   CaptureGL
   ----------
 
-  The webcam class is an simple wrapper around the functionalities
-  of the capture library that takes care of all the lower level things
-  like finding the best pixel format, drawing the frames to screen etc..
+  The webcam class is an simple wrapper around the functionalities of the capture 
+  library that takes care of all the lower level things like finding the best pixel 
+  format and drawing the frames to screen etc..
 
-  If you need more lower level control, use the Capture class directly intead.
+  Header only
+  -----------
+  The `CaptureGL` class is header only class. We're using a header only class so you 
+  can include the openGL headers using your own wrapper or system dependent GL file.
+  In one file, we recommend main.cpp, define VIDEOCAPTURE_IMPLEMENTATION before you 
+  include the <videocapture/CaptureGL.h>. 
+  
+  Usage
+  -----
+  
+  ````c++
+      
+      // include here your GL headers (e.g. using GLFW)
+  
+      #define VIDEOCAPTURE_IMPLEMENTATION
+      #include <videocapture/CaptureGL.h>  
+
+  
+      // ...
+
+      
+      ca::CaptureGL capture;
+      
+      if(capture.open(0, 640, 480) < 0) {
+        printf("Error: Can't open the capture device.\n");
+        ::exit(EXIT_FAILURE);
+      }
+    
+      while(rendering) {
+        capture.update();
+        capture.draw(10, 10, 320, 240);
+      }
+    
+      capture.stop();
+      capture.close(); 
+  ````      
 
 */
 #ifndef VIDEO_CAPTURE_GL_H
@@ -95,8 +130,9 @@ namespace ca {
     int close();                                                                       /* Close the capture device */
     int start();                                                                       /* Start capturing */ 
     int stop();                                                                        /* Stop capturing */
-    void update();                                                                     /* Call this regularly */
+    int update();                                                                      /* Call this regularly. Returns 1 when we update pixels, -1 on error and else 0. */
     void resize(int w, int h);                                                         /* Call w/h when the viewport resizes */
+    void draw();                                                                       /* Draw, filling the the viewport */
     void draw(int x, int y, int w, int h);                                             /* Draw the current frame at x,y and w/h */
 
   private:
@@ -111,6 +147,8 @@ namespace ca {
     int height;                                                                        /* The height of the frames we capture */
     float inv_win_w;                                                                   /* Inverse window/viewport width, used to calculate the correct position/offset values for the vertex shader . */
     float inv_win_h;                                                                   /* Inverse window/viewport height, used to calculate the correct position/offset values for the vertex shader . */
+    int win_w;
+    int win_h;
     Frame frame;                                                                       /* The `Frame` object is used to get information about width/height/bytes used for the current pixel format */ 
     Capture cap;                                                                       /* We use the Capture class for all capture related things */
     GLuint frag;                                                                       /* The fragment shader that does the conversion */
@@ -127,12 +165,7 @@ namespace ca {
   // Update the YUYV422 Pixels
   inline void CaptureGL::updateYUYV422() {
     glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
-    
-#if defined(__APPLE__)
     glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, frame.width[0], frame.height[0], GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, pixels);
-#elif defined(__linux)
-    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, frame.width[0], frame.height[0], GL_RGBA, GL_UNSIGNED_INT_8_8_8_8, pixels);
-#endif
   }
 
 
@@ -170,9 +203,14 @@ namespace ca {
     ,fmt(CA_NONE)
     ,width(0)
     ,height(0)
+    ,inv_win_w(0)
+    ,inv_win_h(0)
+    ,win_w(0)
+    ,win_h(0)
     ,frag(0)
     ,vert(0)
     ,prog(0)
+    ,vao(0)
     ,u_pos(0)
     ,u_tex0(0)
     ,tex0(0)
@@ -189,6 +227,14 @@ namespace ca {
     }
 
     needs_update = false;
+    width = 0;
+    height = 0;
+    inv_win_w = 0.0f;
+    inv_win_h = 0.0f;
+    u_pos = 0;
+    u_tex0 = 0;
+    
+    // @todo - free GL 
   }
 
   int CaptureGL::open(int device, int w, int h) {
@@ -199,12 +245,14 @@ namespace ca {
     Settings settings;
     settings.device = device;
 
+    //cap.listCapabilities(device);
+
     // find the best matching capability
     int caps[] = { CA_YUYV422, CA_YUV420P } ;
     int num_caps = 2;
     for(int i = 0; i < num_caps; ++i) {
       settings.capability = cap.findCapability(device, width, height, caps[i]);
-      if(settings.capability > 0) {
+      if(settings.capability >= 0) {
         fmt = caps[i];
         break;
       }
@@ -255,7 +303,7 @@ namespace ca {
     return cap.start();
   }
 
-  void CaptureGL::update() {
+  int CaptureGL::update() {
     
     if(needs_update) {
 
@@ -267,6 +315,8 @@ namespace ca {
     }
 
     cap.update();
+
+    return needs_update;
   }
 
   int CaptureGL::stop() {
@@ -336,13 +386,7 @@ namespace ca {
 
       glGenTextures(1, &tex0);
       glBindTexture(GL_TEXTURE_2D, tex0);
-
-#if defined(__APPLE__)
       glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, frame.width[0], frame.height[0], 0, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, pixels);
-#elif defined(__linux)
-      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, frame.width[0], frame.height[0], 0, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8, pixels);
-#endif
-
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
@@ -353,6 +397,10 @@ namespace ca {
     }
 
     return  1;
+  }
+
+  void CaptureGL::draw() {
+    draw(0, 0, win_w, win_h);
   }
 
   void CaptureGL::draw(int x, int y, int w, int h) {
@@ -375,6 +423,8 @@ namespace ca {
   void CaptureGL::resize(int w, int h) {
     inv_win_w = 1.0/w;
     inv_win_h = 1.0/h;
+    win_w = w;
+    win_h = h;
   }
 
 #endif // #if defined(VIDEO_CAPTURE_IMPLEMENTATION)
