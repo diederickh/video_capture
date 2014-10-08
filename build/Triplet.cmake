648f8d9459cd5dfd28d0f2a9e8753ceee7b5f36d
roxlu <diederick@apollomedia.nl>
2014-10-08 21:28:45 +0200

    fixes in build scripts

--- build/Triplet.cmake
+++ build/Triplet.cmake
@@ -48,10 +48,11 @@ set(extern_lib_dir ${CMAKE_CURRENT_LIST_DIR}/../extern/${tri_triplet}/lib/)
 set(extern_include_dir ${CMAKE_CURRENT_LIST_DIR}/../extern/${tri_triplet}/include)
 
 
-if ("${CMAKE_INSTALL_PREFIX}" STREQUAL "")
-  set(CMAKE_INSTALL_PREFIX ${install_dir})
-endif()
+#if (CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+#  set(CMAKE_INSTALL_PREFIX ${install_dir})
+#endif()
 
+message(STATUS "${CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT}")
 message(STATUS "Install dir: ${CMAKE_INSTALL_PREFIX}")
 message(STATUS "Building for ${tri_triplet}")
 message(STATUS "Extern include dir: ${extern_include_dir}")
--- build/release.sh
+++ build/release.sh
@@ -6,10 +6,17 @@ if [ ! -d build.release ] ; then
     mkdir build.release
 fi
 
+# set the install dir.
+id=""
+if [ "$(uname)" == "Darwin" ] ; then 
+    id=./../../install/mac-clang-x86_64/bin/
+fi
+
 cd build.release
-cmake -DCMAKE_BUILD_TYPE=Release ../
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${id} ../
 cmake --build . --target install
+
 #./videocapture
 #./opengl_example
-./easy_opengl_example
 #./api_example
+./easy_opengl_example
