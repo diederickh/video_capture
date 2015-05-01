4604ab9a95e32a7ef02cfd5b79ee50a91f8516ae
roxlu <diederick@apollomedia.nl>
2015-05-01 23:47:11 +0200

    Fixed line endings which were somehow switched to windows :#

--- src/easy_opengl_example.cpp
+++ src/easy_opengl_example.cpp
@@ -49,9 +49,9 @@ int main() {
  
   glfwWindowHint(GLFW_SAMPLES, 4);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-  //  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   
   GLFWwindow* win = NULL;
   int w = 1280;
